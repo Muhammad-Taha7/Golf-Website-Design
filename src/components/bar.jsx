5c3fa2c634b69b734a8ba6c66630d0048e187cac
Taha Zahid <muhammadtahazahidrasheed@gmail.com>
2025-09-01 15:39:26 +0500

    done

--- src/components/bar.jsx
+++ src/components/bar.jsx
@@ -2,7 +2,7 @@ export const Bar = () => {
   return (
     <div className="bar-section bg-[#A3D421] text-black w-full py-12 px-6 flex items-center justify-center">
       <div className="max-w-6xl text-center font-semibold leading-snug">
-        <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold">
+        <h2 className="text-2xl sm:text-3xl md:text-3xl lg:text-3xl xl:text-4xl font-bold">
           Sign up for Sidcup News and Special Offers <br className="hidden md:block" />
           Straight to Your Inbox
         </h2>
